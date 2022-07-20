da363c60051a70dd8af2008c821ffd28741fd31a
nami <nami2528@naver.com>
2022-07-20 17:49:14 +0900

    contents

--- pertt/src/main/java/kr/contents/vo/ContentsVO.java
+++ pertt/src/main/java/kr/contents/vo/ContentsVO.java
@@ -1,5 +1,96 @@
 package kr.contents.vo;
 
+import java.sql.Date;
+
 public class ContentsVO {
+	private int c_num;
+	private String title;
+	private String poster;
+	private Date release;
+	private String country;
+	private String genre;
+	private int tomato;
+	private String plot;
+	private String produce;
+	private int grade;
+	
+	private int category_num; //카테고리
+	private int ott_num; //OTT
+	
+	
+	public int getC_num() {
+		return c_num;
+	}
+	public void setC_num(int c_num) {
+		this.c_num = c_num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public Date getRelease() {
+		return release;
+	}
+	public void setRelease(Date release) {
+		this.release = release;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public int getTomato() {
+		return tomato;
+	}
+	public void setTomato(int tomato) {
+		this.tomato = tomato;
+	}
+	public String getPlot() {
+		return plot;
+	}
+	public void setPlot(String plot) {
+		this.plot = plot;
+	}
+	public String getProduce() {
+		return produce;
+	}
+	public void setProduce(String produce) {
+		this.produce = produce;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public int getCategory_num() {
+		return category_num;
+	}
+	public void setCategory_num(int category_num) {
+		this.category_num = category_num;
+	}
+	public int getOtt_num() {
+		return ott_num;
+	}
+	public void setOtt_num(int ott_num) {
+		this.ott_num = ott_num;
+	}
+	
+	
 
 }
