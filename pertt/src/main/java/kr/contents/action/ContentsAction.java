6cb9cf552afeeee9776ba7d456089b5feee8abcf
soooobbbin <soobinlee716@gmail.com>
2022-07-20 19:40:36 +0900

    contensAction 경로 변경

--- pertt/src/main/java/kr/contents/action/ContentsAction.java
+++ pertt/src/main/java/kr/contents/action/ContentsAction.java
@@ -1,5 +1,16 @@
 package kr.contents.action;
 
-public class ContentsAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+
+public class ContentsAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
