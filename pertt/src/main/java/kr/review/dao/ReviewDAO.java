1f4bcee93905e95c91fa32f19f2c7b903e1c0727
heyjon1984 <heyjon1984@gmail.com>
2022-07-21 15:50:36 +0900

    reviewDAO 수정

--- pertt/src/main/java/kr/review/dao/ReviewDAO.java
+++ pertt/src/main/java/kr/review/dao/ReviewDAO.java
@@ -3,6 +3,7 @@ package kr.review.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
 
 import kr.review.vo.ReviewVO;
 import kr.util.DBUtil;
@@ -51,13 +52,21 @@ public class ReviewDAO {
 		}
 	}
 	//한 작품에 담긴 총 리뷰 수
-	public int getReviewCount() throws Exception {
+	public int getReviewCount(int c_num) throws Exception {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql = null;
 		int count = 0;
 		try {
+			conn = DBUtil.getConnection();
+			sql = "select count(*) from c_review where c_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, c_num);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				count = rs.getInt(1);
+			}
 		} catch (Exception e) {
 			throw new Exception(e);
 		} finally {
@@ -66,8 +75,50 @@ public class ReviewDAO {
 		return count;
 	}
 	
-	//글 목록(추천순, 댓글순, 최신순 정렬)
-	
+	//글 목록(최신순(1) 추천순(2) 댓글순(3) 정렬)
+	public int getReviewList(String keyfield) throws Exception {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		String sub_sql = "";
+		List<ReviewVO> list = null;
+		int count = 0;
+	/*	try {
+			conn = DBUtil.getConnection();
+			if(keyfield.equals("1")) sub_sql = "order by c_review_reg_date";
+			else if(keyfield.equals("2")) sub_sql = "order by like";
+			else if(keyfield.equals("3")) {
+				sql = "select count(com_num) from (select * from c_review where c_num=1) "
+					+ "join c_review_com using(c_review_num);";
+			}
+			
+			"select * from (select a.*, rownum rnum "
+			+ "from (select * from zboard b join zmember m "
+			+ "using (mem_num) join zmember_detail d "
+			+ "using (mem_num) " + sub_sql 
+			+ " order by b.board_num desc)a) "
+			+ "where rnum >=? and rnum <= ?";
+			
+			sql = "select * from (select a.*, rownum rnum "
+					+ "from (select * from c_review " +sub_sql +")a) "
+					+ "where rnum >=1 and rnum <= 10";
+			
+			sub_sql ="order by c_review_reg_date";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, c_num);
+			rs = pstmt.executeQuery(); 
+			if(rs.next()) {
+				count = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			throw new Exception(e);
+		} finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		*/
+		return count;
+	}
 	//글 상세 
 	
 	//글 수정 
