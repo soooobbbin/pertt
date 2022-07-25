dd41ee94493ee6a3cc0d3a3ebb4241c127fa73c4
soooobbbin <soobinlee716@gmail.com>
2022-07-25 15:35:44 +0900

    관리자 작품등록

--- pertt/src/main/java/kr/member/action/AdminRegisterContentsAction.java
+++ pertt/src/main/java/kr/member/action/AdminRegisterContentsAction.java
@@ -2,31 +2,45 @@ package kr.member.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.oreilly.servlet.MultipartRequest;
+
 
 import kr.contents.dao.ContentsDAO;
 import kr.contents.vo.ContentsVO;
 import kr.controller.Action;
+import kr.util.FileUtil;
 
 public class AdminRegisterContentsAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		HttpSession session = request.getSession();
+		Integer member_num = 
+				(Integer)session.getAttribute("member_num");
+		if(member_num == null) {//로그인이 되지 않은 경우
+			return "redirect:/member/loginForm.do";
+		}
+		
 		//전송된 데이터 인코딩 처리
 		request.setCharacterEncoding("utf-8");
 		
-		//자바빈(VO) 생성
-		ContentsVO contents =  new ContentsVO();
-		//전송된 데이터를 자바빈에 저장
-		contents.setTitle(request.getParameter("title"));
-		//contents.setRelease(request.getParameter("release"));
-		contents.setCountry(request.getParameter("country"));
-		contents.setGenre(request.getParameter("genre"));
-		//contents.setTomato(request.getParameter("tomato"));
-		contents.setPlot(request.getParameter("plot"));
-		contents.setProduce(request.getParameter("produce"));
-		contents.setGrade(0);
-		contents.setCategory_num(0);
-		contents.setOtt_num(0);
+		//로그인 된 경우
+		MultipartRequest multi = FileUtil.createFile(request);
+		ContentsVO contents = new ContentsVO();
+		contents.setTitle(multi.getParameter("title"));
+		contents.setPoster(multi.getFilesystemName("poster"));
+		//contents.setRelease(multi.getParameter("release"));
+		contents.setCountry(multi.getParameter("country"));
+		contents.setGenre(multi.getParameter("genre"));
+		contents.setTomato(Integer.parseInt(multi.getParameter("tomato")));
+		contents.setPlot(multi.getParameter("plot"));
+		contents.setProduce(multi.getParameter("produce"));
+		contents.setGrade(Integer.parseInt(multi.getParameter("grade")));
+		contents.setCategory_num(Integer.parseInt(multi.getParameter("category_num")));
+		contents.setOtt_num(Integer.parseInt(multi.getParameter("ott_num")));
 		
 		ContentsDAO dao = ContentsDAO.getInstance();
 		dao.insertContents(contents);
